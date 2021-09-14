4552b3c16ceeff2783147dee316a5c93e7a9add7
sptungG <ngoviettung154@gmail.com>
2021-09-14 18:32:15 +0700

    update user and responsive

--- JS/header.js
+++ JS/header.js
@@ -20,18 +20,18 @@ window.addEventListener("load", function () {
   dropdownItems.forEach((el) => el.addEventListener("click", handleSelectDropdown));
 });
 
-//Get the button
-const mybutton = document.getElementById("toTopBtn");
-window.onscroll = function() {scrollFunction()};
+// //Get the button
+// const mybutton = document.getElementById("toTopBtn");
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
