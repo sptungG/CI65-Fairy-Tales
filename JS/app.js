fcc5f1195cc0593b5d750b05e4b210e7ba6a1404
sptungG <ngoviettung154@gmail.com>
2021-09-16 01:09:21 +0700

    update js render

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,11 @@
 import RegisterScreen from "./screens/RegisterScreen.js";
 import LoginScreen from "./screens/LoginScreen.js";
 import { appendTo } from "./models/utils.js";
+import DashBoard from "./screens/DashBoard.js";
 
 let $app = document.getElementById("app");
 const register = new RegisterScreen();
 const login = new LoginScreen();
 // $app.append(login.render());
-appendTo($app, login);
+const dashboard = new DashBoard();
+appendTo($app, dashboard);
---
+++ JS/components/Story.js
@@ -0,0 +1,28 @@
+import BaseComponent from "./BaseComponent.js";
+
+export default class Story extends BaseComponent {
+  render() {
+    let $storyItem = document.createElement("li");
+    $storyItem.classList.add("story", "story-item");
+    $storyItem.innerHTML = `
+    <span class="story-time">${this.props.length}</span>
+    <div class="story-image">
+      <img src="${this.props.coverImage}" alt="" />
+      <div class="author-img--wrapper story-author">
+        <img src="${this.props.authorImage}" alt="" class="author-img" />
+      </div>
+    </div>
+    <div class="story-content">
+      <span class="story-by">${this.props.authorName}</span>
+      <h3 class="story-title">${this.props.name}</h3>
+
+      <div class="story-view">
+        <span class="story-view-num">${this.props.view} views</span>
+        <span class="story-date">${this.props.date}</span>
+      </div>
+      <div class="story-line active"><i class="far fa-heart"></i><span class="heart-num">${this.props.rating}</span></div>
+    </div>
+    `;
+    return $storyItem;
+  }
+}
