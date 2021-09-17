f3d7e3a7e355a493b95f6c5c1ce3ac0bb1cc753a
sptungG <ngoviettung154@gmail.com>
2021-09-17 20:26:45 +0700

    update detail

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,16 @@
 import RegisterScreen from "./screens/RegisterScreen.js";
 import LoginScreen from "./screens/LoginScreen.js";
-import { appendTo } from "./models/utils.js";
 import DashBoard from "./screens/DashBoard.js";
 import Profile from "./screens/Profile.js";
+import { appendTo } from "./models/utils.js";
 
 let $app = document.getElementById("app");
 const register = new RegisterScreen();
 const login = new LoginScreen();
-// $app.append(login.render());
 const dashboard = new DashBoard();
 const profile = new Profile();
-appendTo($app, profile);
+
+// appendTo($app, login);
+// appendTo($app, register);
+// appendTo($app, dashboard);
+// appendTo($app, profile);
