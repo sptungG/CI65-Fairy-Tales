28667c6985fa7b5455ca06123a28aec632c7375e
sptungG <ngoviettung154@gmail.com>
2021-09-20 01:22:39 +0700

    a

--- JS/app.js
+++ JS/app.js
@@ -2,21 +2,14 @@ import RegisterScreen from "./screens/RegisterScreen.js";
 import LoginScreen from "./screens/LoginScreen.js";
 import DashBoard from "./screens/DashBoard.js";
 import Profile from "./screens/Profile.js";
-<<<<<<< HEAD
-import detail from "./screens/detail.js";
-=======
 import { appendTo } from "./models/utils.js";
 import StoryScreen from "./screens/StoryScreen.js";
->>>>>>> dash-board
 
 let $app = document.getElementById("app");
 const register = new RegisterScreen();
 const login = new LoginScreen();
 const dashboard = new DashBoard();
 const profile = new Profile();
-<<<<<<< HEAD
-appendTo($app, new detail());
-=======
 const story = new StoryScreen();
 
 
@@ -25,4 +18,3 @@ const story = new StoryScreen();
 // appendTo($app, dashboard);
 // appendTo($app, profile);
 appendTo($app, story);
->>>>>>> dash-board
