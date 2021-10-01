1f9662ebd5ccb0a9188c52d899cb6773f388d788
sptungG <ngoviettung154@gmail.com>
2021-10-02 01:30:06 +0700

    update btn

--- JS/screens/Header.js
+++ JS/screens/Header.js
@@ -207,17 +207,17 @@ export default class Header extends BaseComponent {
   }
 }
 // //Get the button
-// const mybutton = document.getElementById("toTopBtn");
-// window.onscroll = function() {scrollFunction()};
+const mybutton = document.getElementById("toTopBtn");
+window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
